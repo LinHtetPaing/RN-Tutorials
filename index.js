752ec55813bd7a0f18a66f5ebc110a7f6222c4ac
zwe-mun-htun <zwemunhtun@ucsy.edu.mm>
2018-05-16 03:38:14 +0630

    BlinkText

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import { AppRegistry } from 'react-native';
 import HelloWorld from './components/HelloWorld';
 import CatImage from './components/CatImage';
 import MultipleGreeting from './components/MultipleGreeting';
+import BlinkText from './components/BlinkText';
 
-AppRegistry.registerComponent('RNTutorials', () => MultipleGreeting);
+AppRegistry.registerComponent('RNTutorials', () => BlinkText);
